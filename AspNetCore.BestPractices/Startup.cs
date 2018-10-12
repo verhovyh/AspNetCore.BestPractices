71cf1bae7ddc2f5ac83d0b02093acc3f2f153396
Oleg Verhovyh <Oleg.Verhovyh@itechart-group.com>
2018-10-12 14:02:35 +0300

    Initial commit

--- AspNetCore.BestPractices/Startup.cs
+++ AspNetCore.BestPractices/Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using AspNetCore.BestPractices.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace AspNetCore.BestPractices
 {
@@ -33,6 +35,10 @@ namespace AspNetCore.BestPractices
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            var connection = @"Server=(localdb)\mssqllocaldb;Database=EFGetStarted.AspNetCore.NewDb;Trusted_Connection=True;ConnectRetryCount=0";
+            services.AddDbContext<BloggingContext>
+                (options => options.UseSqlServer(connection));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
